c8895bc12dc1c97c3230d24dfe1f21876373f2eb
taufiq-azr <taufiqdewa91@gmail.com>
2024-11-13 09:22:25 +0700

    project init

---
+++ src/main/java/com/order_backend_application/order_product/OrderProductApplication.java
@@ -0,0 +1,13 @@
+package com.order_backend_application.order_product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderProductApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/order_backend_application/order_product/OrderProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.order_backend_application.order_product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
